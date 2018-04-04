f4b3b1308fd991cda28026d670c77d0ebb22ff9f
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-03 21:34:35 -0400

    All plotting in the same grid, but it is there

--- plot_languages.py
+++ plot_languages.py
@@ -1,4 +1,8 @@
-from ggplot import ggplot
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pylab
 
 import sys
 import pandas
@@ -11,4 +15,8 @@ if __name__ == '__main__':
     df['ngram'] = df['ngram'].replace(to_replace=re.compile(r' \(All\)'), value='')
     df = df.drop(columns=['smoothing', 'type'])
     df = df.set_index(['corpus', 'ngram'])
-    print(df)
+    print(df.T)
+
+    sns.set_style("darkgrid")
+    plt.plot(df.T)
+    plt.savefig('/tmp/lines.png')
