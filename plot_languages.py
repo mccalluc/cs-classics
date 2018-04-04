25657764928c9dcfa91051ca49cbdd84a8692a7f
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-04 18:12:42 -0400

    6 subplots!

--- plot_languages.py
+++ plot_languages.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pylab
@@ -16,11 +15,18 @@ if __name__ == '__main__':
     df['ngram'] = df['ngram'].replace(to_replace=re.compile(r' \(All\)'), value='')
     df = df.drop(columns=['smoothing', 'type'])
     df = df.set_index(['corpus', 'ngram'])
-    print(df.T)
+    #print(df.T)
 
     sns.set_style("darkgrid")
-    fig, ax = plt.subplots(1, 1)
-    ax.set_xticklabels(['' if int(i) % 5 else i for i in df.T.index.tolist()])
+    # fig, ax = plt.subplots(1, 1)
+    every_fifth = ['' if int(i) % 5 else i for i in df.T.index.tolist()]
+    # ax.set_xticklabels(['' if int(i) % 5 else i for i in df.T.index.tolist()])
 
-    plt.plot(df.T)
-    plt.savefig('/tmp/lines.png')
+    filters = ['American English', 'British English', 'Spanish',
+               'French', 'German', 'Italian']
+    for (i, filter) in enumerate(filters):
+        ax = plt.subplot(len(filters), 1, i+1)
+        ax.set_xticklabels(every_fifth)
+        plt.plot(df.loc[filter].T)
+
+    plt.savefig('/tmp/lines.svg')
