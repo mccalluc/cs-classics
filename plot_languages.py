7f4f8f563c75c4aa431264c3e7cae836b89e3a42
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-04 19:10:08 -0400

    Dashed lines

--- plot_languages.py
+++ plot_languages.py
@@ -8,7 +8,7 @@ from matplotlib.ticker import FormatStrFormatter
 import sys
 import pandas
 import re
-
+from cycler import cycler
 
 if __name__ == '__main__':
     df = pandas.read_csv(sys.argv[1], sep='\t')
@@ -23,15 +23,18 @@ if __name__ == '__main__':
     every_fifth = ['' if int(i) % 5 else i for i in df.T.index.tolist()]
     # ax.set_xticklabels(['' if int(i) % 5 else i for i in df.T.index.tolist()])
 
+    plt.rc('axes', prop_cycle=(cycler('linestyle', ['--', '-', ':'])))
     f = plt.figure(figsize=(6, 8))
     filters = ['American English', 'British English', 'Spanish',
                'French', 'German', 'Italian']
     for (i, filter) in enumerate(filters):
         ax = f.add_subplot(len(filters), 1, i+1)
         ax.set_xticklabels(every_fifth)
-        ax.yaxis.set_major_formatter(FormatStrFormatter('%.2e'))
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.1e'))
         plt.title(filter)
         plt.plot(df.loc[filter].T)
+        if i == 0:
+            plt.legend(['Algol', 'Fortran', 'Lisp'])
 
     plt.subplots_adjust(hspace=0.7)
     plt.savefig('/tmp/lines.svg')
