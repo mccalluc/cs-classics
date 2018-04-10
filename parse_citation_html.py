93a31b10ffa84ca858d2cf30ea19a95ba340a894
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-09 21:22:23 -0400

    Raw html from worldcat

---
+++ parse_citation_html.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+def parse(html):
+    pairs = re.search(r'id="raw_tc_data">\{(.*?)\}', html).group(1)
+    d = {year: count for (year, count) in
+               [pair.split('=') for pair in pairs.split(', ')]
+         }
+    return d
+
+if __name__ == '__main__':
+    first = True
+    for path in sys.argv[1:]:
+        with open(path) as f:
+            html = f.read()
+            d = parse(html)
+            if first:
+                print('\t'.join([''] + list(d)))
+                first = False
+            print('\t'.join([path] + list(d.values())))
